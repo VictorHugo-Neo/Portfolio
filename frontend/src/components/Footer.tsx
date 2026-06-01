0109dd5c7e5ad04396d7af0bb66812a53811541b
VictorHugo-Neo <prfvhlc@gmail.com>
2026-05-31 23:40:16 -0300

    styles: atualização de informação no footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="max-w-7xl mx-auto px-6 py-16 border-t border-black/5 dark:border-white/5 text-center">
             <div className="text-[10px] font-mono uppercase tracking-[0.5em] opacity-30">
-                Victor Hugo Lisboa Coutinho • 2026 • OpenSUSE Environment
+                Victor Hugo Lisboa Coutinho • 2026
             </div>
         </footer>
     )
